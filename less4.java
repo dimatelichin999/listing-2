2c9e9ff5ce083ac4d8149855a3ea862bc986792f
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-18 21:58:51 +0200

    Підрахунок чисел в трьохзначні

--- less4.java
+++ less4.java
@@ -1,2 +1,20 @@
-public class less4 {
+import java.util.Scanner;
+class less4 {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ведіть 4 значне число");
+        int amount = input.nextInt();
+        for (int i = 1; i <= 4 ; i++) {
+            for (int j = 1; j <= 4 ; j++) {
+                for (int k = 1; k <= 4 ; k++) {
+                    if (k !=1 && k != j && i != j){
+                        amount++;
+                        System.out.println(i + "" + j + " + k");
+                    }
+                }
+            }
+            System.out.println("Підрахунок з числел  трьохзначні числа є" + amount);
+        }
+    }
 }
